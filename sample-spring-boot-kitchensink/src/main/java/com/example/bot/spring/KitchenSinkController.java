a4f765e51de8508c3c59a86c83dfdf557f429801
Ridho <ridhoadhadigani@gmail.com>
2017-07-15 07:23:31 +0700

    Update KitchenSinkController.java

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -390,7 +390,7 @@ public class KitchenSinkController {
             }
             case "imagemap":
                 this.reply(replyToken, new ImagemapMessage(
-                        createUri("/static/rich/S012.jpg"),
+                        createUri("/static/rich/S012.jpeg"),
                         "This is alt text",
                         new ImagemapBaseSize(1040, 1040),
                         Arrays.asList(
